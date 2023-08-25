2bb8ad2e52bcf5a391ea52cd44c541f0ecda87b4
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-08-25 12:21:22 +0900

    ユーザー管理_コメントアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,4 @@
 class UsersController < ApplicationController
 
-  def create
-    @user = User.new(params.require(:user).permit(:email, :password, :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana))
-    @user.save
-  end
 
 end
