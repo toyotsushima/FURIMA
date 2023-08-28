cd7205f1eeab456a1bdd3756c743df8ea241c03d
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-08-28 18:20:01 +0900

    新規登録・ログイン・ログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  "before_action :authenticate_user!"
+  #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :basic_auth
 
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, :encrypted_password])
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
-
+  def create
+    @user = User.new(params.require(:user).permit(:email, :password, :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday))
+    @user.save
+  end
 
 end
