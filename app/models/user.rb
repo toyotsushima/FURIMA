2b3f8a01aae0cc60eee076600072b5d5e4b2b824
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-08-29 15:38:54 +0900

    ユーザーモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true
-  validates :nickname, presence: true
+  validates :nickname, presence: true, length: { maximum: 40 }
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :first_name_kana, presence: true
   validates :last_name_kana, presence: true
   validates :birthday, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  
 end
