4dbebde5e56658abbbbba1e32f05b75de8ea403d
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-08-29 21:13:10 +0900

    Rubocopを実行

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,7 @@ FactoryBot.define do
     end
     nickname              { Faker::Name.initials(number: 2) }
     email                 { Faker::Internet.email }
-    password              { '1a' +Faker::Internet.password(min_length: 6) }
+    password              { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     first_name            { person.first.kanji }
     last_name             { person.last.kanji }
