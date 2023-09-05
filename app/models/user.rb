49dd9fb15f6661e908030f641085e68dcde901d8
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-09-05 16:00:20 +0900

    orderモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :items
+  has_many :oder
 
   validates :nickname, presence: true, length: { maximum: 40 }
   validates :first_name, presence: true
