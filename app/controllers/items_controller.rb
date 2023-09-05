d6e983cd8755e3d21d39d0570df5a70dbb091eb8
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-09-05 14:47:06 +0900

    指定箇所修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,8 @@
 class ItemsController < ApplicationController
-
   before_action :authenticate_user!, only: :new
 
   def index
-    @items = Item.all
+    # @items = Item.all
   end
 
   def new
@@ -15,14 +14,14 @@ class ItemsController < ApplicationController
     if @item.save
       redirect_to root_path
     else
-      render :new,status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :item_info, :category_id, :item_status_id, :shipping_fee_status_id, :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_info, :category_id, :item_status_id, :shipping_fee_status_id,
+                                 :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
-
 end
