63bf46977b8203f4cd850e45fca63dae05de9b73
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-09-08 14:25:53 +0900

    ルーティング、コントローラー設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -22,10 +22,30 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    redirect_to root_path unless current_user.id == @item.user_id
+  end
+
+  def update
+    @item = Item.find(params[:id])
+  if @item.update(item_params)
+    redirect_to item_path(item_params)
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+
   private
 
   def item_params
     params.require(:item).permit(:image, :item_name, :item_info, :category_id, :item_status_id, :shipping_fee_status_id,
                                  :prefecture_id, :delivery_date_id, :price).merge(user_id: current_user.id)
   end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
+  end
+
 end
