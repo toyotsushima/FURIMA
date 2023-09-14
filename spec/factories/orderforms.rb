811703249285a2fceb2d78f35f2a6055d06f9fba
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-09-14 18:32:23 +0900

    購入者情報モデル単体テスト

---
+++ spec/factories/orderforms.rb
@@ -0,0 +1,12 @@
+FactoryBot.define do
+  factory :order_form do
+      item_id { 1 }
+      user_id { 1 }
+      postal_code { '111-1111' }
+      prefecture_id { 2 }
+      city { '横浜市緑区' }
+      address { '青山1-1-1' }
+      building { '柳ビル103' }
+      phone_number { Faker::Number.decimal_part(digits: 11) }
+  end
+end
---
+++ spec/models/orderform_spec.rb
@@ -0,0 +1,77 @@
+require 'rails_helper'
+
+RSpec.describe OrderForm, type: :model do
+    before do
+      @orderform = FactoryBot.build(:order_form)
+    end
+
+    describe '購入者情報の保存' do
+      context '購入者情報の保存ができるとき' do
+        it 'すべての値が正しく入力されていれば保存できる' do
+          expect(@orderform).to be_valid
+        end
+        it 'buildingが空でも保存できる' do
+          @orderform.building = nil
+          expect(@orderform).to be_valid
+        end
+      end
+  
+      context '購入者情報の保存ができないとき' do
+        it 'user_idが空だと保存できない' do
+          @orderform.user_id = nil
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("User can't be blank")
+        end
+        it 'item_idが空だと保存できない' do
+          @orderform.item_id = nil
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("Item can't be blank")
+        end
+        it 'postal_codeが空だと保存できない' do
+          @orderform.postal_code = nil
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("Postal code can't be blank", "Postal code is invalid. Include hyphen(-)")
+        end
+        it 'postal_codeにハイフンがないと保存できない' do
+          @orderform.postal_code = 1234567
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        end
+        it 'postal_codeが3桁-4桁でなければ保存できない' do
+          @orderform.postal_code = 1234-56789
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+        end
+        it 'prefecture_idが未選択だと出品できない' do
+          @orderform.prefecture_id = 0
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("Prefecture can't be blank")
+        end
+        it 'cityが空だと保存できない' do
+          @orderform.city = nil
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("City can't be blank")
+        end
+        it 'addressが空だと保存できない' do
+          @orderform.address = nil
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("Address can't be blank")
+        end
+        it 'phone_numberが空だと保存できない' do
+          @orderform.phone_number = nil
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include("Phone number can't be blank")
+        end
+        it 'phone_numberにハイフンがあると保存できない' do
+          @orderform.phone_number = '090-1234-5678'
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include('Phone number is invalid')
+        end
+        it 'phone_numberが12桁以上あると保存できない' do
+          @orderform.phone_number = 123456789101234
+          @orderform.valid?
+          expect(@orderform.errors.full_messages).to include('Phone number is invalid')
+        end
+      end
+    end
+end
