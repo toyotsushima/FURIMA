f9ac7af74bed9262559dfa81f7fa6b35b4ecfc1f
toyotsushima <rstoyo.aika.tsushima@gmail.com>
2023-09-26 12:57:32 +0900

    バリデーション修正

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -11,7 +11,7 @@ class OrderForm
     validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
     validates :token
   end
-  validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
   def save
     order = Order.create(user_id:, item_id:)
